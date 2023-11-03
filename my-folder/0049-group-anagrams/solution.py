c8fef45d07726743fea363646eb8126d7fa55523
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2023-11-03 15:44:00 +0000

    [LeetCode Sync] Runtime - 93 ms (42.86%), Memory - 20.5 MB (44.09%)

---
+++ my-folder/0049-group-anagrams/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dd = defaultdict(list)
+        for i in strs:
+            ss = tuple(sorted(i))
+            dd[ss].append(i)
+        return dd.values()
+        
+        
