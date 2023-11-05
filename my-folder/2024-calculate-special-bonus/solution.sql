afa9e949f7ad9109d8b7377258298b4115dd9ac9
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2023-11-05 17:11:36 +0000

    [LeetCode Sync] Runtime - 569 ms (7.07%), Memory - 0B (100.00%)

---
+++ my-folder/2024-calculate-special-bonus/solution.sql
@@ -0,0 +1,12 @@
+-- Write your PostgreSQL query statement below
+-- Write your PostgreSQL query statement below
+select 
+    employee_id,
+    (case
+        when name like 'M%' then 0
+        when mod(employee_id, 2) = 0 then 0
+        else salary
+    end) as bonus
+
+from employees
+order by employee_id
