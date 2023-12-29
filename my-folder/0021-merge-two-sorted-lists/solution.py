8938cce22f902cc96f4c377d358a17c957fcb42e
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2023-12-29 21:12:40 +0000

    [LeetCode Sync] Runtime - 53 ms (5.52%), Memory - 17.4 MB (5.34%)

--- my-folder/0021-merge-two-sorted-lists/solution.py
+++ my-folder/0021-merge-two-sorted-lists/solution.py
@@ -17,7 +17,6 @@ class Solution:
         if list1.val < list2.val:
             list1.next = self.mergeTwoLists(list1.next, list2)
             return list1
-
         else:
             list2.next = self.mergeTwoLists(list1, list2.next)
             return list2
