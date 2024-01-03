1debd5b1d0ca75cc7a520e7f6354df50e1ed7421
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-01-03 13:51:29 +0000

    [LeetCode Sync] Runtime - 1561 ms (57.83%), Memory - 65.3 MB (20.97%)

---
+++ my-folder/0211-design-add-and-search-words-data-structure/solution.py
@@ -0,0 +1,45 @@
+class TreeNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TreeNode()
+        
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = TreeNode()
+            cur = cur.children[char]
+
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+
+        def dfs(j, root):
+            cur = root
+            for i in range(j, len(word)):
+                char = word[i]
+                if char == '.':
+                    for child in cur.children.values():
+                        if dfs(i+1, child):
+                            return True
+                    return False
+                else:
+                    if char not in cur.children:
+                        return False
+                    cur = cur.children[char]
+
+            return cur.endOfWord
+
+        return dfs(0, self.root)
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
