1f9d969f12445424042635a4b13e9c4de070a97f
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-01-06 19:44:30 +0000

    [LeetCode Sync] Runtime - 43 ms (10.06%), Memory - 17.4 MB (7.58%)

---
+++ my-folder/0198-house-robber/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        def dp(i):
+            if i == 0:
+                return nums[0]
+
+            if i == 1:
+                return max(nums[0], nums[1])
+
+            if i not in cache:
+                cache[i] = max(dp(i-1), dp(i-2) + nums[i])
+            
+            return cache[i]
+
+        cache = {}
+        return dp(len(nums) - 1)
+        
