701a697b9b21cdfeb95c50f897495ff6b754db65
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-01-19 14:25:31 +0000

    [LeetCode Sync] Runtime - 132 ms (47.06%), Memory - 26.3 MB (16.41%)

--- my-folder/0967-minimum-falling-path-sum/solution.py
+++ my-folder/0967-minimum-falling-path-sum/solution.py
@@ -1,17 +1,20 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+
         @lru_cache(None)
         def dp(r, c):
-            ## base cases:
+
+            ## base cases
             if c >= n or c < 0:
-                return float('inf')
+                return inf
+
             if r == m-1:
                 return matrix[r][c]
 
-            ## recurances
-            return matrix[r][c] +  min(dp(r+1, c+1), dp(r+1, c), dp(r+1, c-1))
+            ## recursive iteration
+            return matrix[r][c] + min(dp(r+1, c-1), dp(r+1, c), dp(r+1, c+1))
 
-
+        inf = 10 ** 10
         m, n = len(matrix), len(matrix[0])
         return min([dp(0, j) for j in range(n)])
         
