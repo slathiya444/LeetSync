d23b98567365ef8fb8dbabf2a091df8023ccf758
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-01-21 13:48:37 +0000

    [LeetCode Sync] Runtime - 38 ms (41.53%), Memory - 16.5 MB (30.79%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,9 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        first, second = 0, 0
-        for i in nums:
-            max_profit = max(first + i, second)
-            first = second
-            second = max_profit
-        return second
-        
+        res = [0] * (len(nums))
+        res[0] = nums[0]
+        if len(nums) == 1:
+            return res[0]
+        res[1] = max(nums[0], nums[1])
+        if len(nums) ==2:
+            return res[1]
+        for i in range(2, len(nums)):
+            res[i] = max(res[i-1], res[i-2]+nums[i])
+
+        return res[-1]
