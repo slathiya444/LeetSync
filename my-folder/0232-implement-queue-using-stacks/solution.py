35a7a89734f846db0582a83135e7019fdff8918b
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-01-29 15:43:52 +0000

    [LeetCode Sync] Runtime - 35 ms (52.75%), Memory - 16.6 MB (72.92%)

--- my-folder/0232-implement-queue-using-stacks/solution.py
+++ my-folder/0232-implement-queue-using-stacks/solution.py
@@ -1,19 +1,31 @@
 class MyQueue:
 
     def __init__(self):
-        self.stack = []
+        self.push_stack = []
+        self.pop_stack = []
+    
+    def helper_push(self):
+        while self.push_stack:
+            self.pop_stack.append(self.push_stack.pop())
 
     def push(self, x: int) -> None:
-        self.stack.append(x)
+        self.push_stack.append(x)
 
     def pop(self) -> int:
-        return self.stack.pop(0)
+        # if pop stack is empty, 
+        #check push stack and push all elements from push stack to pop stack in reverse
+        # so that pop operation will end up being O(1)
+        if not self.pop_stack:
+            self.helper_push()
+        return self.pop_stack.pop()
 
     def peek(self) -> int:
-        return self.stack[0]
+        if not self.pop_stack:
+            self.helper_push()
+        return self.pop_stack[-1]
 
     def empty(self) -> bool:
-        return len(self.stack) == 0
+        return len(self.push_stack) == 0 and len(self.pop_stack) == 0
 
 
 # Your MyQueue object will be instantiated and called as such:
