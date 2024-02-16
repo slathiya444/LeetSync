e0cc505c4b99655e0977318d1fe906e0f9a0cf1f
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-02-16 02:11:56 +0000

    [LeetCode Sync] Runtime - 334 ms (68.97%), Memory - 33.6 MB (47.58%)

---
+++ my-folder/1604-least-number-of-unique-integers-after-k-removals/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        # count = collections.Counter(arr)
+        count = Counter(arr)
+        
+        freq = sorted(list(count.values()))
+        print(freq)
+        idx = 0
+        while idx <= len(freq)-1 and freq[idx] <= k :
+            k -= freq[idx]
+            idx += 1
+            
+
+        return len(freq) - idx
