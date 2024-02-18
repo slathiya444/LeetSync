fca12f8bc08482740cdd632f55cdceae2ca617dd
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-02-18 16:22:44 +0000

    [LeetCode Sync] Runtime - 82 ms (15.67%), Memory - 19.8 MB (90.26%)

---
+++ my-folder/0253-meeting-rooms-ii/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        if not intervals:
+            return 0
+        
+        intervals.sort(key = lambda x: x[0])
+
+        free_rooms = []
+        heapq.heappush(free_rooms, intervals[0][1])
+
+        for curr in intervals[1:]:
+            if curr[0] >= free_rooms[0]:
+                heapq.heappop(free_rooms)
+
+            heapq.heappush(free_rooms, curr[1])
+
+        return len(free_rooms)
+        
