b931af24bb975c3091c9a3609d5b58bdadffd8ed
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-02-26 00:08:51 +0000

    [LeetCode Sync] Runtime - 31 ms (79.63%), Memory - 16.5 MB (42.13%)

--- my-folder/0100-same-tree/solution.py
+++ my-folder/0100-same-tree/solution.py
@@ -9,9 +9,9 @@ class Solution:
 
         if not p and not q:
             return True
-        
-        if not p or not q or p.val != q.val:
-            return False
 
-        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+        if p and q and p.val == q.val:
+            return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+
+        else: return False
         
