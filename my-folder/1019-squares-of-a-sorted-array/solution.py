0dc2ca8367c558da2d32ea8b14a396d28f32dc2d
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-03-02 17:18:44 +0000

    [LeetCode Sync] Runtime - 143 ms (95.92%), Memory - 19 MB (12.75%)

--- my-folder/1019-squares-of-a-sorted-array/solution.py
+++ my-folder/1019-squares-of-a-sorted-array/solution.py
@@ -1,8 +1,18 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        l = []
-        for i in nums:
-            l.append(i*i)
-        
-        return sorted(l)
+        ## as the input is sorted, zero should be in middle because of negetive values,
+        ## but square values are the same (eg. (-6) and 6 have the same value 36)
+        ## so we can start from highest square values
+        res = [] # return will be reverse of this
+        left, right = 0, len(nums)-1
+        while left <= right:
+            if abs(nums[left]) >= abs(nums[right]):
+                res.append(nums[left] ** 2)
+                left += 1
+            else:
+                res.append(nums[right] ** 2)
+                right -= 1
+
+        return res[::-1]
+
         
