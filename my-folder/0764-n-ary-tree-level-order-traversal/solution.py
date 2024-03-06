10b28de93bde3e891f15cdd76803fdc2ce25d9af
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-03-06 12:54:03 +0000

    [LeetCode Sync] Runtime - 48 ms (46.15%), Memory - 18.2 MB (79.26%)

---
+++ my-folder/0764-n-ary-tree-level-order-traversal/solution.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if root is None:
+            return []
+        q = deque([root])
+        res = []
+        while q:
+            level = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                level.append(node.val)
+
+                q.extend(node.children)
+
+            res.append(level)
+
+        return res
+
+        
