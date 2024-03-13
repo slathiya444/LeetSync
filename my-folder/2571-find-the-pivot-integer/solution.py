48bf7e3d45b8cc471d11c4b9590089d7c80ec900
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-03-13 00:55:26 +0000

    [LeetCode Sync] Runtime - 49 ms (51.37%), Memory - 16.5 MB (32.89%)

---
+++ my-folder/2571-find-the-pivot-integer/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        left_value = 1
+        right_value = n
+        sum_left = left_value
+        sum_right = right_value
+
+        if n == 1:
+            return n
+
+        # Iterate until the pointers meet
+        while left_value < right_value:
+            # Adjust sums and pointers based on comparison
+            if sum_left < sum_right:
+                sum_left += left_value + 1
+                left_value += 1
+            else:
+                sum_right += right_value - 1
+                right_value -= 1
+
+            # Check for pivot condition
+            if sum_left == sum_right and left_value + 1 == right_value - 1:
+                return left_value + 1
+
+        return -1  # Return -1 if no pivot is found
