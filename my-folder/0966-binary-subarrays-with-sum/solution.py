198a49f2a0a4f4c1b5b2b24dc8cd3a23a9d2b240
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-03-14 13:58:05 +0000

    [LeetCode Sync] Runtime - 226 ms (41.39%), Memory - 17.2 MB (96.79%)

---
+++ my-folder/0966-binary-subarrays-with-sum/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+
+        def helper(required_total):
+            if required_total < 0:
+                return 0
+
+            res = 0
+            left, current_sum = 0, 0
+
+            for right in range(len(nums)):
+                current_sum += nums[right]
+                while current_sum > required_total:
+                    current_sum -= nums[left]
+                    left += 1
+                res += (right - left + 1)
+
+            return res
+
+        return helper(goal) - helper(goal-1)
