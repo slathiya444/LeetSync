5e7263e47b3dc73e3bbfbdc21c3085ba9b8a63e9
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-03-25 14:59:06 +0000

    [LeetCode Sync] Runtime - 261 ms (61.11%), Memory - 24.8 MB (60.65%)

---
+++ my-folder/0442-find-all-duplicates-in-an-array/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        res = []
+
+        for num in nums:
+            num = abs(num)
+            if nums[num-1] < 0:
+                res.append(num)
+            nums[num-1] *= -1
+        return res
+        
