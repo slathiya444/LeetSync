053cc7ef18a7bb12807f4ffb27ad58ed021b9db9
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-04-24 13:30:45 +0000

    [LeetCode Sync] Runtime - 43 ms (7.28%), Memory - 16.5 MB (64.87%)

--- my-folder/1236-n-th-tribonacci-number/solution.py
+++ my-folder/1236-n-th-tribonacci-number/solution.py
@@ -2,11 +2,15 @@ class Solution:
     def tribonacci(self, n: int) -> int:
 
         def dp(i):
+
+            ## base cases
             if i in [0,1]:
                 return i
 
             if i == 2:
                 return 1
+
+            ## recursive call
             if i not in cache:
                 cache[i] = res = dp(i-1) + dp(i-2) + dp(i-3)
             return cache[i]
