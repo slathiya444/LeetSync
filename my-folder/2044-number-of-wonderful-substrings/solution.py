deac189f0c78e56350218cd5b4652ddeb7d7943f
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-04-30 14:12:42 +0000

    [LeetCode Sync] Runtime - 207 ms (98.58%), Memory - 17.4 MB (88.62%)

---
+++ my-folder/2044-number-of-wonderful-substrings/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def wonderfulSubstrings(self, word: str) -> int:
+        toBit = {c: 1 << i for i, c in enumerate(ascii_lowercase[:10])}
+        mask = 0
+
+        count = defaultdict(int)
+        count[0] = 1
+
+        for c in word:
+            mask ^= toBit[c]
+            count[mask] += 1
+
+        res = 0
+        for mask, cnt in count.items():
+            res += cnt * (cnt - 1) // 2
+            for i in range(10):
+                mask2 = mask ^ (1 << i)
+                if mask2 < mask:
+                    res += cnt * count.get(mask2, 0)
+            
+
+        
+        return res
+
+      
+            
