af86488d95e80c54d25931f84e4b3e39b021a097
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-05-04 11:45:47 +0000

    [LeetCode Sync] Runtime - 34 ms (63.75%), Memory - 16.7 MB (6.15%)

---
+++ my-folder/0165-compare-version-numbers/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        i = j = 0
+
+        while(i < len(version1) or j < len(version2)):
+            v1 = v2 = 0
+
+            while (i < len(version1) and version1[i] != '.'):
+                v1 = v1 * 10 + int(version1[i])
+                i += 1
+            while (j < len(version2) and version2[j] != '.'):
+                v2 = v2 * 10 + int(version2[j])
+                j += 1
+
+            if v1 > v2:
+                return 1
+            elif v1 < v2:
+                return -1
+
+            i, j = i + 1, j + 1
+
+        return 0
