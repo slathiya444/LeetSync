479dc0c7d78506ec3a17a37d215bd172de4e95e1
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-05-06 13:54:22 +0000

    [LeetCode Sync] Runtime - 517 ms (25.13%), Memory - 60.9 MB (15.51%)

---
+++ my-folder/2573-remove-nodes-from-linked-list/solution.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        stack = []
+        current = head
+
+        # Add nodes to the stack
+        while current:
+            stack.append(current)
+            current = current.next
+
+        current = stack.pop()
+        maximum = current.val
+        result_list = ListNode(maximum)
+
+        # Remove nodes from the stack and add to result
+        while stack:
+            current = stack.pop()
+            # Current should not be added to the result
+            if current.val < maximum:
+                continue
+            # Add new node with current's value to front of the result
+            else:
+                new_node = ListNode(current.val)
+                new_node.next = result_list
+                result_list = new_node
+                maximum = current.val
+
+        return result_list
+        
