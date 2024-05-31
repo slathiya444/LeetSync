85bcfc4f509b6f703a9c6e0ca95a0e39a57e93d5
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2024-05-31 03:28:43 +0000

    [LeetCode Sync] Runtime - 63 ms (32.67%), Memory - 18.7 MB (39.47%)

---
+++ my-folder/0260-single-number-iii/solution.py
@@ -0,0 +1,5 @@
+from collections import Counter
+class Solution:
+    def singleNumber(self, nums: int) -> List[int]:
+        hashmap = Counter(nums)
+        return [x for x in hashmap if hashmap[x] == 1]
